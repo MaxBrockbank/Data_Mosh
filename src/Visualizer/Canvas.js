69c88c6b6ec4bf1b245a9ef35319c70e0efbf36f
Max Brockbank <maxbrockbank1999@gmail.com>
2021-02-26 15:10:20 -0800

    successfully play single hardcoded song

--- src/Visualizer/Canvas.js
+++ src/Visualizer/Canvas.js
@@ -1,20 +1,37 @@
 import React from 'react'
 import songFile from './../Audio/BILLYJOEL.mp3';
+import * as BS from 'react-icons/bs'
 
 class Canvas extends React.Component{
   constructor(props){
     super(props);
     this.audio = new Audio(songFile);
-  }
-
-  togglePlay(){
-    if(this.audio.paused){
-      this.audio.play()
-    } else {
-      this.audio.paused();
+    this.state = {
+      buttonIcon:<BS.BsPlayFill/>
     }
   }
 
+  togglePlay = () => {
+    if(this.audio.paused){
+      this.audio.play()
+      this.setState({
+        buttonIcon:<BS.BsPauseFill/>
+      })
+    } else {
+      this.audio.pause();
+      this.setState({
+        buttonIcon:<BS.BsPlayFill/>
+      })
+    }
+  }
+
+  render (){
+    return(
+      <>
+        <button onClick={this.togglePlay}>{this.state.buttonIcon}</button>
+      </>
+    );
+  }
 }
 
 export default Canvas
